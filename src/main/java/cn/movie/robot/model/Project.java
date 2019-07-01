44df99c938556feef94341302c0612b962b20cef
wuxiaoyi <wuxiaoyi@fishtrip.cn>
2019-07-01 14:43:43 +0800

    -

---
+++ src/main/java/cn/movie/robot/model/Project.java
@@ -0,0 +1,11 @@
+package cn.movie.robot.model;
+
+/**
+ * @author Wuxiaoyi
+ * @date 2019/7/1
+ */
+public class Project {
+  private Integer id;
+  private Integer sid;
+
+}
---
+++ src/main/java/cn/movie/robot/model/ProjectDetail.java
@@ -0,0 +1,8 @@
+package cn.movie.robot.model;
+
+/**
+ * @author Wuxiaoyi
+ * @date 2019/7/1
+ */
+public class ProjectDetail {
+}
